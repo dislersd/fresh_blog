adfa1fbb316347e105512e947285cc14cec3d641
dislersd <dislersd@gmail.com>
2022-08-19 13:19:00 -0700

    render markdown

--- routes/blog/[id].tsx
+++ routes/blog/[id].tsx
@@ -3,6 +3,8 @@ import { h } from "preact";
 import { Handlers, PageProps } from '$fresh/server.ts'
 import { tw } from "@twind";
 import { loadPost, Post } from "../../utils/posts.ts";
+import * as gfm from "https://deno.land/x/gfm@0.1.22/mod.ts";
+
 
 export const handler: Handlers<Post> = {
   async GET(req, ctx) {
@@ -17,11 +19,13 @@ export const handler: Handlers<Post> = {
 
 export default function BlogPostPage(props: PageProps) {
   const post = props.data
+  const html = gfm.render(post.content)
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
       <p class={tw`text-gray-600 mt-12`}>{post.publishAt.toLocaleDateString()}</p>
       <h1 class={tw`text-5xl mt-5 font-bold`} >{post.title}</h1>
-      <div class={tw`mt-12`}>{post.content}</div>
+      <style dangerouslySetInnerHTML={{ __html: gfm.CSS }} />
+      <div class={tw`mt-12` + " markdown-body"} dangerouslySetInnerHTML={{ __html: html }} />
     </div>
   )
 }
