62fd6509fdfa7247449d1b8df254b6227aac846e
dislersd <dislersd@gmail.com>
2022-08-19 20:39:16 -0700

    fresh blog

---
+++ constants.ts
@@ -0,0 +1,3 @@
+export const COPY = {
+  BLOG_TITLE: "Fresh Blog",
+};
--- routes/index.tsx
+++ routes/index.tsx
@@ -3,6 +3,7 @@ import { h } from "preact";
 import { tw } from "@twind";
 import { listPosts, Post } from "../utils/posts.ts";
 import { Handlers, PageProps } from "https://deno.land/x/fresh@1.0.2/server.ts";
+import { COPY } from "@constants";
 
 export const handler: Handlers<Post[]> = {
   async GET(_req, ctx) {
@@ -15,7 +16,7 @@ export default function Home(props: PageProps<Post[]>) {
   const posts = props.data
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <h1 class={tw`text-5xl mt-12 font-bold`} >Dylan's Blog</h1>
+      <h1 class={tw`text-5xl mt-12 font-bold bg-blue-100 p-4 w-full`} >{COPY.BLOG_TITLE}</h1>
       <ul class={tw`mt-8`}>
         {posts.map(post => <PostEntry post={post} />)}
       </ul>
